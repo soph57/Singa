0ad7dfd64f50b3abc4886d1dfa85785dde1c0e08
ASA\Andre <anoliveiasa@hotmail.com>
2017-08-14 16:29:34 +0200

    correction des imports

--- singa-ang/src/app/app.module.ts
+++ singa-ang/src/app/app.module.ts
@@ -8,23 +8,23 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {TranslateLoader, TranslateModule} from '@ngx-translate/core';
 import {TranslateHttpLoader} from '@ngx-translate/http-loader';
 import {AppComponent} from './app.component';
-import { NavbarComponent } from './Components/src/app/Components/navbar/navbar.component';
-import { SubscriptionComponent } from './Components/src/app/Components/subscription/subscription.component';
-import { ProgrammesComponent } from './Components/src/app/Components/programmes/programmes.component';
-import { AnnouncementsComponent } from './Components/src/app/Components/announcements/announcements.component';
-import { StoriesComponent } from './Components/src/app/Components/stories/stories.component';
-import { LocationsComponent } from './Components/src/app/Components/locations/locations.component';
-import { FooterComponent } from './Components/src/app/Components/footer/footer.component';
-import { SidebarComponent } from './Components/src/app/Components/sidebar/sidebar.component';
-import { SubscriptionPageComponent } from './Components/src/app/Components/subscription-page/subscription-page.component';
-import { StoriesPageComponent } from './Components/src/app/Components/stories-page/stories-page.component';
-import { StoriesAddComponent } from './Components/src/app/Components/stories-add/stories-add.component';
-import { ProfileNavComponent } from './Components/src/app/Components/profile-nav/profile-nav.component';
-import { ProfileInfoComponent } from './Components/src/app/Components/profile-info/profile-info.component';
-import { ProfilMeetSuggestsComponent } from './Components/src/app/Components/profil-meet-suggests/profil-meet-suggests.component';
-import { ProfileProposalsComponent } from './Components/src/app/Components/profile-proposals/profile-proposals.component';
-import { ProfilGroupComponent } from './Components/src/app/Components/profil-group/profil-group.component';
-import { EventsComponent } from './Components/src/app/Components/events/events.component';
+import { NavbarComponent } from './Components/navbar/navbar.component';
+import { SubscriptionComponent } from './Components/subscription/subscription.component';
+import { ProgrammesComponent } from './Components/programmes/programmes.component';
+import { AnnouncementsComponent } from './Components/announcements/announcements.component';
+import { StoriesComponent } from './Components/stories/stories.component';
+import { LocationsComponent } from './Components/locations/locations.component';
+import { FooterComponent } from './Components/footer/footer.component';
+import { SidebarComponent } from './Components/sidebar/sidebar.component';
+import { SubscriptionPageComponent } from './Components/subscription-page/subscription-page.component';
+import { StoriesPageComponent } from './Components/stories-page/stories-page.component';
+import { StoriesAddComponent } from './Components/stories-add/stories-add.component';
+import { ProfileNavComponent } from './Components/profile-nav/profile-nav.component';
+import { ProfileInfoComponent } from './Components/profile-info/profile-info.component';
+import { ProfilMeetSuggestsComponent } from './Components/profile-meet-suggests/profil-meet-suggests.component';
+import { ProfileProposalsComponent } from './Components/profile-proposals/profile-proposals.component';
+import { ProfileGroupComponent } from './Components/profile-group/profile-group.component';
+import { EventsComponent } from './Components/events/events.component';
 
 export function HttpLoaderFactory(http: HttpClient) {
   return new TranslateHttpLoader(http, './assets/i18n/', '.json');
@@ -48,7 +48,7 @@ export function HttpLoaderFactory(http: HttpClient) {
     ProfileInfoComponent,
     ProfilMeetSuggestsComponent,
     ProfileProposalsComponent,
-    ProfilGroupComponent,
+    ProfileGroupComponent,
     EventsComponent
   ],
   imports: [
